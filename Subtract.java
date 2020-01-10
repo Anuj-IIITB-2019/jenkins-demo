9ae523c9f80ad43548f1ae946dcc4cf43d4f3df0
Anuj-IIITB-2019 <53935371+Anuj-IIITB-2019@users.noreply.github.com>
2020-01-10 11:46:02 +0530

    Create Subtract.java

---
+++ Subtract.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Subtract{
+	public static void main(String[] args) 
+	{
+	float a,b,res;
+	char choice;
+	scanner sc = new Scanner(System.in);
+	do{
+	System.out.println("1 Subtract Two numbers");
+	System.out.println("2. Exit \n\n");
+	System.out.println("Enter your choice");
+	choice  = scan.next().charAt(0);
+	switch(choice)
+		{
+		case '1': System.out.println("Enter Two Numbers");
+		a=scan.nextFloat();
+		b = scan.nextFloat();
+		if(b==0.00){System.out.println("Invalid Operation");
+		break;}
+		res = a-b;
+		System.out.println("Result = "+ res );
+		break;
+		case '2': System.exit(0);break;
+
+		default: System.out.println("Invalid Choice");
+		break;
+		}
+		System.out.print("\n ---------------------------------------------- \n");
+	}
+	System.out.print("press y to exit");
+	while(ch !='y');
+
+	}
+}
